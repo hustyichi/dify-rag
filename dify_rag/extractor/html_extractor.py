ae2fc1518d6c71b7b415d9b1cd4cc9a11601cf7f
libing <libing@healink.cn>
2024-08-21 16:18:19 +0800

    MOD: use new html_text

--- dify_rag/extractor/html_extractor.py
+++ dify_rag/extractor/html_extractor.py
@@ -5,8 +5,6 @@ from dify_rag.extractor import html_text, utils
 from dify_rag.extractor.extractor_base import BaseExtractor
 from dify_rag.models.document import Document
 
-NO_TITLE = "[no-title]"
-
 
 class HtmlExtractor(BaseExtractor):
     def __init__(
@@ -14,10 +12,14 @@ class HtmlExtractor(BaseExtractor):
         file_path: str,
         remove_hyperlinks: bool = True,
         fix_check: bool = True,
+        contain_closest_title_levels: int = 0,
+        title_convert_to_markdown: bool = False,
     ) -> None:
         self._file_path = file_path
         self._remove_hyperlinks = remove_hyperlinks
         self._fix_check = fix_check
+        self._contain_closest_title_levels = contain_closest_title_levels
+        self._title_convert_to_markdown = title_convert_to_markdown
 
     @staticmethod
     def convert_table_to_markdown(table) -> str:
@@ -67,6 +69,52 @@ class HtmlExtractor(BaseExtractor):
 
         return str(soup), tables_md
 
+    @staticmethod
+    def convert_to_markdown(html_tag: str, title: str) -> str:
+        if not (title and html_tag):
+            return title
+
+        html_tag = html_tag.lower()
+        if html_tag.startswith("h") and html_tag[1:].isdigit():
+            level = int(html_tag[1])
+            return f'{"#" * level} {title}'
+        else:
+            return title
+
+    def trans_titles_and_content(
+        self, content: str, titles: list[tuple[str, str]]
+    ) -> str:
+        titles = titles[-self._contain_closest_title_levels :]
+        if self._contain_closest_title_levels == 0:
+            titles = []
+
+        if not content:
+            return content
+
+        trans_content = ""
+        for tag, title in titles:
+            if not title:
+                continue
+
+            if self._title_convert_to_markdown:
+                title = HtmlExtractor.convert_to_markdown(tag, title)
+
+            trans_content += f"{title}\n"
+        trans_content += content
+        return trans_content
+
+    def trans_meta_titles(self, titles: list[tuple[str, str]]):
+        trans_titles = []
+        for tag, title in titles:
+            if not title:
+                continue
+
+            if self._title_convert_to_markdown:
+                title = HtmlExtractor.convert_to_markdown(tag, title)
+
+            trans_titles.append(title)
+        return trans_titles
+
     def extract(self) -> list[Document]:
         with open(
             self._file_path, "r", encoding=utils.get_encoding(self._file_path)
@@ -77,22 +125,20 @@ class HtmlExtractor(BaseExtractor):
             text, tables = self.preprocessing(text)
 
             html_doc = readability.Document(text)
-            content = html_text.extract_text(html_doc.summary(html_partial=True))
+            content, split_contents, titles = html_text.extract_text(
+                html_doc.summary(html_partial=True), title=html_doc.title()
+            )
 
-            title = html_doc.title()
-            if title != NO_TITLE:
-                txt = f"{title}\n{content}"
-            else:
-                txt = content
-
-            sections = txt.split("\n")
-            clean_sections = []
-            for sec in sections:
-                sec = sec.strip()
-                if sec:
-                    clean_sections.append(sec)
-
-            docs = [Document(page_content="\n\n".join(clean_sections))]
+            docs = []
+            for content, hierarchy_titles in zip(split_contents, titles):
+                docs.append(
+                    Document(
+                        page_content=self.trans_titles_and_content(
+                            content, hierarchy_titles
+                        ),
+                        metadata={"titles": self.trans_meta_titles(hierarchy_titles)},
+                    )
+                )
 
             for table in tables:
                 docs.append(Document(page_content=table))
