2192e9c6e27049a2a15e921fce4d3702a99d25a0
Cling_o3 <45124798+ProseGuys@users.noreply.github.com>
2024-08-28 14:44:23 +0800

    [chore] upgrade retrieval  (#25)

--- dify_rag/retrieval/document_retrieval.py
+++ dify_rag/retrieval/document_retrieval.py
@@ -7,7 +7,9 @@ from dify_rag.models.document import Document
 
 
 def retrieval2reorganize(
-    query_docs: list[Document], current_docs_segemts: dict[str : list[Document]]
+    query_docs: list[Document],
+    current_docs_segemts: dict[str : list[Document]],
+    max_token: int = 500,
 ) -> list[Document]:
     """Reorganize extracted content
 
@@ -18,61 +20,73 @@ def retrieval2reorganize(
     Returns:
         list[Document]: _description_
     """
-    query_docs_map = {}
+    query_docs_title_map = {}
     query_metadata_map = {}
     title_map = {}
     final_documents_list = []
     for doc in query_docs:
         document_id = doc.metadata.get("document_id")
-        title, _ = doc.page_content.split(CUSTOM_SEP)
-        query_docs_map[document_id] = query_docs_map.get(document_id, set())
-        query_docs_map[document_id].add(title)
-        query_metadata_map[f"{document_id}_{title}"] = doc.metadata
+        title = content = doc.page_content
+        if CUSTOM_SEP in doc.page_content:
+            title, content = doc.page_content.split(CUSTOM_SEP)
+        query_docs_title_map[document_id] = query_docs_title_map.get(document_id, set())
+        query_docs_title_map[document_id].add(title)
+        key = f"{document_id}_{title}"
+        if key not in query_metadata_map:
+            query_metadata_map[key] = {"metadata": {}, "content": ""}
+        query_metadata_map[key]["metadata"] = doc.metadata
+        query_metadata_map[key]["content"] = content
 
     for document_id, docs in current_docs_segemts.items():
-        titles = query_docs_map.get(document_id)
+        titles = query_docs_title_map.get(document_id)
         for doc in docs:
-            title, content = doc.page_content.split(CUSTOM_SEP)
+            title = content = doc.page_content
+            if CUSTOM_SEP in doc.page_content:
+                title, content = doc.page_content.split(CUSTOM_SEP)
+            key = f"{document_id}_{title}"
             if title in titles:
-                title_map[f"{document_id}_{title}"] = title_map.get(
-                    f"{document_id}_{title}", []
-                )
-                title_map[f"{document_id}_{title}"].append(content)
+                title_map[key] = title_map.get(key, [])
+                title_map[key].append(content)
 
     for key, contents in title_map.items():
+        content = query_metadata_map[key]["content"]
+        new_content = content
+        if len(contents) >= 2:
+            # 需要考虑策略自带的字符补充逻辑
+            content_index = contents.index(content)
+            left, right, target = content_index, content_index, 0
+            start, end = 0, len(contents) - 1
+            while len(new_content) < max_token and (
+                left - 1 >= start or right + 1 <= end
+            ):
+                if (target == 0 and right + 1 <= end) or (
+                    target != 0 and left - 1 >= start
+                ):
+                    right += 1
+                    new_content = splice_contents(new_content, contents[right])
+                    target = 1
+
+                else:
+                    left -= 1
+                    new_content = splice_contents(contents[left], new_content)
+                    target = 0
+
         doc = Document(
-            page_content="".join(contents), metadata=query_metadata_map.get(key, {})
+            page_content=new_content, metadata=query_metadata_map[key]["metadata"]
         )
         final_documents_list.append(doc)
 
     return final_documents_list
 
 
-if __name__ == "__main__":
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import scoped_session, sessionmaker
-    from sqlalchemy.sql import text
-
-    engine = create_engine(
-        "postgresql+psycopg2://postgres:difyai123456@100.100.30.201:5432/dify"
-    )
-    db = scoped_session(sessionmaker(bind=engine))
-
-    query_docs_smtm = "select * from document_segments where document_id = '4c1d1ace-ac7b-4b74-a44d-d1e8bfae3b67' order by position"
-    current_docs = []
-    a = db.execute(text(query_docs_smtm))
-    for doc in a.fetchall():
-        current_docs.append(
-            Document(
-                page_content=doc.content,
-                metadata={"document_id": "4c1d1ace-ac7b-4b74-a44d-d1e8bfae3b67"},
-            )
-        )
-    query_docs = [current_docs[12], current_docs[-1]]
-
-    print(current_docs)
-    print(
-        retrieval2reorganize(
-            query_docs, {"4c1d1ace-ac7b-4b74-a44d-d1e8bfae3b67": current_docs}
-        )
-    )
+def splice_contents(prev: str, next: str):
+    start_char = next[0]
+    prev_right = len(prev) - 1
+    while 0 <= prev_right:
+        if prev[prev_right] == start_char:
+            similar_segment = prev[prev_right:]
+            if similar_segment == next[: len(similar_segment)]:
+                next = next[len(similar_segment) :]
+                break
+        prev_right -= 1
+    return prev + next
