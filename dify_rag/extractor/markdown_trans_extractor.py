bb259393628fd6e870d8b5b50002031f3528f3a3
Pengzirong <Peng.Zirong@outlook.com>
2025-02-13 16:02:18 +0800

    ADD: Support full text extraction for markdown files with marker

--- dify_rag/extractor/markdown_trans_extractor.py
+++ dify_rag/extractor/markdown_trans_extractor.py
@@ -1,11 +1,13 @@
 import os
 import tempfile
+from typing import Optional
 
 import markdown2
 
 from dify_rag.extractor.extractor_base import BaseExtractor
 from dify_rag.extractor.html import constants
 from dify_rag.extractor.html_extractor import HtmlExtractor
+from dify_rag.models import constants as global_constants
 from dify_rag.models.document import Document
 
 
@@ -22,6 +24,7 @@ class MarkdownExtractor(BaseExtractor):
         split_tags: list[str] = constants.SPLIT_TAGS,
         prevent_duplicate_header: bool = True,
         use_summary: bool = False,
+        choices_return_full_text_marker: str = global_constants.CHOICES_RETURN_FULL_TEXT_MARKER,
     ) -> None:
         self._file_path = file_path
         self._html_extractor_params = {
@@ -35,18 +38,29 @@ class MarkdownExtractor(BaseExtractor):
             'prevent_duplicate_header': prevent_duplicate_header,
             'use_summary': use_summary,
         }
+        self._choices_return_full_text_marker = choices_return_full_text_marker
+
+    def _handle_full_text_marker(self, md_content: str) -> Optional[list[Document]]:
+        if not self._choices_return_full_text_marker:
+            return None
+
+        first_line, *rest = md_content.strip().split('\n', 1)
+        return [Document(page_content=rest[0])] if rest and first_line == self._choices_return_full_text_marker else None
 
     def extract(self) -> list[Document]:
-        original_name = os.path.splitext(os.path.basename(self._file_path))[0]
-        temp_dir = tempfile.gettempdir()
-        temp_html_path = os.path.join(temp_dir, f"{original_name}.html")
-
         with open(self._file_path, 'r', encoding='utf-8') as md_file:
             md_content = md_file.read()
+
+        # 如果 md_content 以全文标记开头，则返回全文
+        if docs := self._handle_full_text_marker(md_content):
+            return docs
 
         markdowner = markdown2.Markdown(extras=['tables', 'fenced-code-blocks', 'toc'])
         html_content = markdowner.convert(md_content)
 
+        original_name = os.path.splitext(os.path.basename(self._file_path))[0]
+        temp_dir = tempfile.gettempdir()
+        temp_html_path = os.path.join(temp_dir, f"{original_name}.html")
         with open(temp_html_path, 'w', encoding='utf-8') as temp_html:
             temp_html.write(html_content)
 
--- dify_rag/models/constants.py
+++ dify_rag/models/constants.py
@@ -1,7 +1,7 @@
 import enum
 
 CUSTOM_SEP = "---###---"
-
+CHOICES_RETURN_FULL_TEXT_MARKER = "<!-- CHOICES_RETURN_FULL_TEXT -->"
 
 class ContentType(str, enum.Enum):
     TEXT = "text"
